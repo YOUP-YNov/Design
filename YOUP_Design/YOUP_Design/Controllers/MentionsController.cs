a71f9ad55513581f0ef872c43c8e37f683251999
MatheaR <mathea.renaudie@gmail.com>
2014-11-14 16:26:58 +0100

    Integration mentions legales

---
+++ YOUP_Design/YOUP_Design/Controllers/MentionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YOUP_Design.Controllers
+{
+    public class MentionsController : Controller
+    {
+        //
+        // GET: /Mentions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
