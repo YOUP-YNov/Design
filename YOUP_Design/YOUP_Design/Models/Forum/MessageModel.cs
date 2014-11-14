370660b569304c0073859ea8e7e5e3861ee63a9c
JangoBtz <paul.dubois.64@gmail.com>
2014-11-14 15:34:12 +0100

    Commit des models forum

--- YOUP_Design/YOUP_Design/Models/Forum/MessageModel.cs
+++ YOUP_Design/YOUP_Design/Models/Forum/MessageModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace YOUP_Design.Models.Forum
 {
     public class MessageModel
     {
+        [Required]
+        [Display(Name = "Contenu du message")]
+        public string ContenuMessage { get; set; }
+
     }
 }
--- YOUP_Design/YOUP_Design/Models/Forum/TopicModel.cs
+++ YOUP_Design/YOUP_Design/Models/Forum/TopicModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace YOUP_Design.Models.Forum
 {
     public class TopicModel
     {
+        [Required]
+        [Display(Name = "Nom du topic")]
+        public string Nom { get; set; }
+
+        [Required]
+        [Display(Name = "Description du topic")]
+        public string DescriptionTopic { get; set; }
     }
 }
