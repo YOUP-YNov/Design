548805a0da5bc2093eb86b7d42a3debeabc6087b
jbouchareine <julien.bouchareine@y-nov.com>
2014-11-14 18:17:22 +0100

    ajout méthode de création d'évènements

---
+++ YOUP_Design/YOUP_Design/Classes/Evenement/webApiObjects/EvenementCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YOUP_Design.Classes.Evenement.webApiObjects
+{
+    public class EvenementCreate
+    {
+        public EvenementFront evenement { get; set; }
+        public List<long> friends { get; set; }
+    }
+}
---
+++ YOUP_Design/YOUP_Design/WebApi/Evenement/webApiEvenementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using YOUP_Design.Classes.Evenement.webApiObjects;
+using YOUP_Design.Models.Evenement.webApiObjects;
+
+namespace YOUP_Design.WebApi.Evenement
+{
+    public class webApiEvenementController
+    {
+        public async void PutEvent(EvenementCreate evenement)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://youp-evenementapi.azurewebsites.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // New code:
+                HttpResponseMessage response = await client
+                    .PutAsJsonAsync<EvenementCreate>("api/Evenement", evenement);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("ajout de l'évènement");
+                }
+            }
+        }
+    }
+}
