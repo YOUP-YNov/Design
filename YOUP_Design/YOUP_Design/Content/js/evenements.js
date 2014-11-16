b95f09df52247ec9ec1e18d4f3dba95582099800
jbouchareine <julien.bouchareine@y-nov.com>
2014-11-16 15:49:32 +0100

    ajout récupération évenement et lien vers la page de création.

--- YOUP_Design/YOUP_Design/Content/js/evenements.js
+++ YOUP_Design/YOUP_Design/Content/js/evenements.js
@@ -1,13 +1,80 @@
 ﻿var evenementModule = (function () {
+
+    function listeEvenementDate(data) {
+        var self = this;
+
+        self.date = ko.observable(data.date);
+        self.listeEvenements = ko.observableArray();
+
+        for (var i = 0; i < data.listeEvenements.length; i++)
+            self.listeEvenements.push(new evenement(data.evenements[i]));
+    }
+
+    function evenement(data) {
+        var self = this;
+
+        self.idEvenement = ko.observable(data.Evenement_id);
+        self.titre = ko.observable(data.TitreEvenement);
+        self.imgEvt = ko.observable(function ()
+            {
+                return data.ImgEvt == "" || data.ImgEvt == null ? "~/Images/default_profil.png" : data.ImgEvt;
+            });
+        self.adresse = ko.observable(data.Adresse);
+        self.prix = ko.observable(function () {
+            return data.Prix == 0 ? "gratuit" : data.Prix + " €";
+        });
+        self.description = ko.observable(data.Descrption);
+        self.date = ko.observable(data.DateEvenement);
+        self.ville = ko.observable(data.Ville);
+        self.categorie = ko.observable(data.VilleCategorie);
+        self.nbParticipant = ko.observable(data.nbParticipant);
+        self.nbMaxParticipant = ko.observable(data.nbMaxParticipant);
+        self.imgOrganisateur = ko.observable(data.ImgOrganisateur);
+        self.organisateur = ko.observable(data.Organisateur);
+        self.visible = ko.observable(true);
+
+    }
+
+    var timeLineViewModel = function (data) {
+        var self = this;
+
+        self.listeEvenementDate = ko.observableArray();
+
+        for (var i = 0; i < data.length; i++)
+            self.listeEvenementDate.push(new listeEvenementDate(data[i]));
+    }
+
     var apiUrl = "http://youp-evenementapi.azurewebsites.net/api/";
 
     var recuperationListeEvenements = function () {
+        var today = Date.today();
+        Date.today().addDays(14)
         $.ajax({
             type: "GET",
             url: apiUrl + 'Evenement',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({ 'dateSearch': today }),
             success: function (msg) {
-                var listeEvenements = jQuery.parseJSON(msg);
-                alert("sucess recuperationListeEvenements");
+                var datas = msg;
+                traiterListeEvenement(datas);
+            }
+        });
+    }
+
+    var traiterListeEvenement = function (data) {
+        
+        $.ajax({
+            type: "POST",
+            url: 'Evenements/TimeLine',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(data ),
+            success: function (msg) {
+                var datas = msg.timeLine;
+
+                ko.applyBindings(new timeLineViewModel(datas));
+                $("#timeLineEvent").show();
             }
         });
     }
@@ -25,6 +92,7 @@
     }
     
     var init = function () {
+        $("#timeLineEvent").hide();
         recuperationListeEvenements();
     }
 
---
+++ YOUP_Design/YOUP_Design/Models/Evenement/templatesObjects/evenementTimeLineObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YOUP_Design.Models.Evenement.webApiObjects;
+
+namespace YOUP_Design.Models.Evenement.templatesObjects
+{
+    public class evenementTimeLineObject
+    {
+        public long idEvenement;
+        public string titre;
+        public string imgEvt;
+        public string adresse;
+        public string prix;
+        public string description;
+        public string date;
+        public string ville;
+        public string categorie;
+        public int nbParticipant;
+        public int nbMaxParticipant;
+        public string imgOrganisateur;
+        public string organisateur;
+
+        public evenementTimeLineObject()
+        {
+
+        }
+
+        public evenementTimeLineObject(EvenementTimelineFront evt)
+        {
+            this.idEvenement = evt.Evenement_id;
+            this.titre = evt.TitreEvenement;
+            this.imgEvt = "";
+            this.adresse = evt.Adresse;
+            this.prix = evt.Prix.ToString();
+            this.description = "TODO";
+            this.date = evt.DateEvenement.ToShortDateString();
+            this.ville = evt.Adresse;
+            this.categorie = evt.Categorie_id.ToString();
+            this.nbParticipant = 0;
+            this.nbMaxParticipant = 0;
+            this.imgOrganisateur = "";
+            this.organisateur = "";
+        }
+    }
+}
