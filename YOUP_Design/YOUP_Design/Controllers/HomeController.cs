1c0b98cb8eb3f58d7d24eda350edf9322463055b
deejay-ste <dj-ste_971@live.fr>
2014-11-20 17:49:37 +0100

    Home Controller updated

--- YOUP_Design/YOUP_Design/Controllers/HomeController.cs
+++ YOUP_Design/YOUP_Design/Controllers/HomeController.cs
@@ -3,14 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YOUP_Design.Classes.Blog;
+using YOUP_Design.Classes.Evenement;
+using YOUP_Design.Classes.Profile;
+using YOUP_Design.Classes.Historique;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace YOUP_Design.Controllers
 {
     public class HomeController : Controller
     {
+        public T ProfilRequest<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient("http://aspmoduleprofil.azurewebsites.net/");
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+        public T EventRequest<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient("http://youp-evenementapi.azurewebsites.net/");
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+        public T BlogRequest<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient("http://youp-blog.azurewebsites.net/");
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+        public T HistoriqueRequest<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient("http://youp-evenementapi.azurewebsites.net/");
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+
+        public List<UtilisateurSmall> GetLastProfiles()
+        {
+            var request = new RestRequest("api/TopTenUser", Method.GET);
+            var result = ProfilRequest<List<UtilisateurSmall>>(request);
+            return result;
+        }
+
+        public List<EvenementTimeline> GetLastEvents()
+        {
+            var request = new RestRequest("api/Evenement?max_result=3", Method.GET);
+            var result = EventRequest<List<EvenementTimeline>>(request);
+            return result;
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Home";
+            //Derniers Utilisateurs
+            List<UtilisateurSmall> profiles = new List<UtilisateurSmall>();
+            List<UtilisateurSmall> lastprofiles = new List<UtilisateurSmall>();
+            profiles = this.GetLastProfiles();
+            int count = 0;
+
+            foreach (UtilisateurSmall profil in profiles)
+            {
+                if (count < 4)
+                {
+                    lastprofiles.Add(profil);
+                }
+
+                count++;
+            }
+
+            ViewBag.LastProfiles = lastprofiles;
+
+            //Derniers Evenements
+            List<EvenementTimeline> events = new List<EvenementTimeline>();
+            events = this.GetLastEvents();
+
+            ViewBag.LastEvents = events;
 
             return View();
         }
