c838ceba93c65a3a207de2b873d4eba358eb8014
MartinLG <martin.leguillou@y-nov.com>
2014-11-21 14:11:21 +0100

    start search integration

---
+++ YOUP_Design/YOUP_Design/Classes/Recherche/Source.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YOUP_Design.Classes.Recherche
+{
+    public class Source
+    {
+        public string Id { get; set; }
+
+        public Source()
+        {
+
+        }
+    }
+}
