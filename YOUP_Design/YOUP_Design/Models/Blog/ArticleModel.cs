964e444e9efa29ec00d5a4cc516bcf1b495b6b6e
JangoBtz <paul.dubois.64@gmail.com>
2014-11-29 01:29:14 +0100

    BLOG scope

--- YOUP_Design/YOUP_Design/Models/Blog/ArticleModel.cs
+++ YOUP_Design/YOUP_Design/Models/Blog/ArticleModel.cs
@@ -9,6 +9,9 @@ namespace YOUP_Design.Models.Blog
 {
     public class ArticleModel
     {
+        [Display(Name = "id de l'article")]
+        public int Id { get; set; }
+
         [Required]
         [Display(Name = "Titre de l'article")]
         public string TitreArticle { get; set; }
@@ -25,5 +28,12 @@ namespace YOUP_Design.Models.Blog
         [Display(Name = "Actif")]
         public bool Actif { get; set; }
 
+        [Required]
+        [Display(Name = "Evenement lié à l'article")]
+        public int EventId { get; set; }
+
+        [Required]
+        [Display(Name = "Tags de l'article")]
+        public string Tags { get; set; }
     }
 }
